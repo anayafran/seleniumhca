4fea2e646ed244e9341daf27560c7fd02685894d
anayafran <webdev@anayafran.com>
2016-11-08 13:16:15 -0800

    Update JoinLinkedIn2.java

--- Book Code/Day 02_ Chapters 4-8/Chapter 6/JoinLinkedIn2.java
+++ Book Code/Day 02_ Chapters 4-8/Chapter 6/JoinLinkedIn2.java
@@ -6,6 +6,9 @@ public class JoinLinkedIn2 {
     WebDriver driver;
     @BeforeTest
     public void setUp() throws Exception {
+        //System.setProperty("webdriver.chrome.driver", "C:/SeleniumLabs/LabEnvironment/SeleniumServers/chromedriver.exe");
+        
+        
         driver = new ChromeDriver();
         // Go to LinkedIn’s Home Page
         driver.get("https: //www.linkedin.com/");
