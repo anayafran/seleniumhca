aa9b9d16c4a35f069381059a02b4e103ce0eacb2
anayafran <webdev@anayafran.com>
2016-11-08 20:11:31 +0000

    Update test.java

--- inClassExamples/test.java
+++ inClassExamples/test.java
@@ -1,9 +1,15 @@
 package inClassExamples;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class test {
   @Test
-  public void f() {
+  void createWebDriver()  {
+	  WebDriver driver = new FirefoxDriver();
+	  
+	  driver.
+  
   }
 }
